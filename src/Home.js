1656401eac2552a61ba172ee6b10995d515ccac2
Saroj Karki <karki.saroj28@gmail.com>
2021-04-04 12:46:20 +0545

    Adds Product component

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -7,9 +8,15 @@ function Home() {
       <div className="home__container">
         <img
           className="home__image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Bollywood/1500x600_Hero-Tall_np_bolly._CB405289994_.jpg"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt="amazon"
         />
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row"></div>
+        <div className="home__row"></div>
       </div>
     </div>
   );
---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+        <p className="product__price">
+            <small>$</small>
+            <strong>15.99</strong>
+        </p>
+        <div className="product__rating">
+            <p>⭐</p>
+        </div>
+      </div>
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+      alt="book" />
+      <button>Add to Basket</button>
+    </div>
+  );
+}
+
+export default Product;
