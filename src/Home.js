ec0ab4cb5815bfbacc3ee974e60f60a369ad5918
Saroj Karki <karki.saroj28@gmail.com>
2021-04-04 11:25:17 +0545

    Adds home component

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Bollywood/1500x600_Hero-Tall_np_bolly._CB405289994_.jpg"
+          alt="amazon"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
